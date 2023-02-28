ec05f6014936986160ff21a70ffe3705df809081
JatinSaxena2002 <jatinsaxena2704@gmail.com>
2023-02-28 12:48:41 +0530

    Updates

---
+++ precache-manifest.6d112e14ac424708289bf52b1c1d92ac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b641dfa8bfa01c9f3e1",
+    "url": "/reactlivewebsite/static/css/main.bc6577d4.chunk.css"
+  },
+  {
+    "revision": "0b641dfa8bfa01c9f3e1",
+    "url": "/reactlivewebsite/static/js/main.0b641dfa.chunk.js"
+  },
+  {
+    "revision": "744cc4ce1f2ccf36b4b6",
+    "url": "/reactlivewebsite/static/css/1.8414c029.chunk.css"
+  },
+  {
+    "revision": "744cc4ce1f2ccf36b4b6",
+    "url": "/reactlivewebsite/static/js/1.744cc4ce.chunk.js"
+  },
+  {
+    "revision": "18a195a03cc8ea014908",
+    "url": "/reactlivewebsite/static/js/runtime~main.18a195a0.js"
+  },
+  {
+    "revision": "2f7b2613c808666dca05f4ecea8e193a",
+    "url": "/reactlivewebsite/static/media/homeimg1.2f7b2613.webp"
+  },
+  {
+    "revision": "4661cb8603ae7ff026242f4b84b0fb87",
+    "url": "/reactlivewebsite/static/media/homeimg2.4661cb86.webp"
+  },
+  {
+    "revision": "a5243e8be45e022afc1cccb79416e85b",
+    "url": "/reactlivewebsite/static/media/homeimg10.a5243e8b.webp"
+  },
+  {
+    "revision": "3203965d6ff6145ae3023b516346c132",
+    "url": "/reactlivewebsite/static/media/homeimg12.3203965d.webp"
+  },
+  {
+    "revision": "630feb14fd4b695a502b94ca3e93dd13",
+    "url": "/reactlivewebsite/static/media/homeimg11.630feb14.webp"
+  },
+  {
+    "revision": "189e9a6fa1c557c009fc239408e1f102",
+    "url": "/reactlivewebsite/static/media/homeimg8.189e9a6f.webp"
+  },
+  {
+    "revision": "7e9b656eea8e70478df813806b6d7791",
+    "url": "/reactlivewebsite/static/media/aboutdesign.7e9b656e.png"
+  },
+  {
+    "revision": "afad8b49fe2fb957ca5d8c8eba3f790c",
+    "url": "/reactlivewebsite/static/media/projectintegration.afad8b49.png"
+  },
+  {
+    "revision": "e559c21c2d419649455ea42e86560923",
+    "url": "/reactlivewebsite/static/media/communication.e559c21c.jpg"
+  },
+  {
+    "revision": "bafe3f801f1cfbd4a678890e09adcbe9",
+    "url": "/reactlivewebsite/static/media/design.bafe3f80.jpg"
+  },
+  {
+    "revision": "dce5632842dfefc856aa59c99d58e1f2",
+    "url": "/reactlivewebsite/static/media/customer.dce56328.jpg"
+  },
+  {
+    "revision": "6bcd3d894865118dbf286a5bc151adf0",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
